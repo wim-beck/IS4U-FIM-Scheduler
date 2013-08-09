f5e14328a811cd8ae42c9f0d22daeeff17670205
wim-beck <wim.beck@hotmail.com>
2013-08-09 11:39:34 +0200

    version 1.0
    Add license to source files

--- RunConfiguration/RunJob.cs
+++ RunConfiguration/RunJob.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/**
+ * IS4U's Forefront Identity Manager Scheduler is created to schedule automated 
+ * run profiles using configuration files on the Synchronization Service.
+ * 
+ * Copyright (C) 2013 by IS4U (info@is4u.be)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * A full copy of the GNU General Public License can be found 
+ * here: http://opensource.org/licenses/gpl-3.0.
+ */
+using System;
 using System.IO;
 using System.Management;
 using Microsoft.Win32;
